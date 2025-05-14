b41ccaaf28dab6e314f0f625119abd0a50ec892c
manugomez1206 <manugomezgallego26@gmail.com>
2025-05-14 17:33:48 -0500

    Correción de carpeta en layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./styles/globals.css";
+import "./globals.css";
 import { AuthContextProvider } from "./context/AuthContext";
 
 const geistSans = Geist({
